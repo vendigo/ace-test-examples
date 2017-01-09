bcb22ce8b52cc2a1a57a8c9faff601cd30f7c765
vendigo <ace.vendigo@gmail.com>
2017-01-09 18:16:51 +0200

    Divided into two modules: end-to-end and with-mocks

--- end-to-end/src/test/java/cucumber/CucumberTest.java
+++ end-to-end/src/test/java/cucumber/CucumberTest.java
@@ -1,4 +1,4 @@
-package com.github.vendigo.examples.acetest;
+package cucumber;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = {"cucumber.api.spring", "com.github.vendigo.acetest.cucumber"},
-        features = {"classpath:TwoLaunchers.feature"},
         plugin = {"pretty", "html:target/cucumber"})
 public class CucumberTest {
 }
